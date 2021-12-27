ac926e4e473b630f33b27ddc9cf7736315644b1e
vasilii <porokh.vs@gmail.com>
2021-12-27 03:10:29 +0300

    process

--- src/_CORE/entities/createPlayer_v02.js
+++ src/_CORE/entities/createPlayer_v02.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import { Object3D } from 'three'
 
 
 export class Player {
@@ -12,66 +13,63 @@ export class Player {
             frontObjPos,
             backObjPos,
             lightDataOne,
-            speed,
-            offsetFromFloor,
-            offsetFromFloorFactor,
-            offsetWallCollision,
-            speedDown,
-            speedRot,
         } = CONSTANTS.playerConfig
 
 
         this._camera = null
         this._isBlocked = true
 
+        this.mesh = new THREE.Object3D()
+        //this.mesh = new THREE.Mesh(
+        //    new THREE.BoxGeometry(.5, .5, .5),
+        //    new THREE.MeshBasicMaterial({ color: 0xffff00 }) 
+        //)
+        this.mesh.position.fromArray([0, 20, 0])
+        this.mesh.rotation.fromArray([0, 0, 0])
+        this.mesh.userData.type = 'player'
 
-        this._mainObj = new THREE.Mesh(
-            new THREE.BoxGeometry(.5, .5, .5),
-            new THREE.MeshBasicMaterial({ color: 0xffff00 }) 
-        )
-        this._mainObj.position.fromArray([0, 0, 0])
-        this._mainObj.rotation.fromArray([0, 0, 0])
-        this._mainObj.userData.type = 'player'
 
-
-        this.bottomObj = new THREE.Mesh(
-            new THREE.BoxGeometry(.5, .5, .5),
-            new THREE.MeshBasicMaterial({ color: 0x000000 }) 
-        )
+        this.bottomObj = new THREE.Object3D()
+        // this.bottomObj = new THREE.Mesh(
+        //     new THREE.BoxGeometry(.5, .5, .5),
+        //     new THREE.MeshBasicMaterial({ color: 0x000000 }) 
+        // )
         this.bottomObj.position.fromArray([0, -2, 0])
-        this._mainObj.add(this.bottomObj)
+        this.mesh.add(this.bottomObj)
 
-        this.frontObj = new THREE.Mesh(
-            new THREE.BoxGeometry(.5, .5, .5),
-            new THREE.MeshBasicMaterial({ color: 0xFFFFFF }) 
-        )
+        this.frontObj = new THREE.Object3D()
+        // this.frontObj = new THREE.Mesh(
+        //     new THREE.BoxGeometry(.5, .5, .5),
+        //     new THREE.MeshBasicMaterial({ color: 0xFFFFFF }) 
+        // )
         this.frontObj.position.fromArray([0, 0, -2])
-        this._mainObj.add(this.frontObj)
+        this.mesh.add(this.frontObj)
 
-        this.backObj = new THREE.Mesh(
-            new THREE.BoxGeometry(.5, .5, .5),
-            new THREE.MeshBasicMaterial({ color: 0xff0000 }) 
-        )
+        this.backObj = new THREE.Object3D()
+        // this.backObj = new THREE.Mesh(
+        //     new THREE.BoxGeometry(.5, .5, .5),
+        //     new THREE.MeshBasicMaterial({ color: 0xff0000 }) 
+        // )
         this.backObj.position.fromArray([0, 0, 2])
-        this._mainObj.add(this.backObj)
+        this.mesh.add(this.backObj)
 
 
         {
             const { fov, ratio, near, far, pos } = cameraData
             this._camera = new THREE.PerspectiveCamera(fov, ratio, near, far)
             this._camera.position.fromArray([0, 0, 2])
-            this._mainObj.add(this._camera)
+            this.mesh.add(this._camera)
         }
 
         {
             const { color, strenth, pos } = lightDataOne
             const light = new THREE.PointLight(color, strenth)
             light.position.fromArray(pos)
-            this._mainObj.add(light)
+            this.mesh.add(light)
         }
 
         studio.setCamera(this._camera)
-        studio.addToScene(this._mainObj)
+        studio.addToScene(this.mesh)
     }
 
     toggleBlocked (val) {
@@ -83,7 +81,7 @@ export class Player {
     }
 
     getObj () {
-        return this._mainObj
+        return this.mesh
     }
 
     getCamera () {
@@ -91,6 +89,6 @@ export class Player {
     }
     
     setToPos (x, y, z) {
-        this._mainObj.position.set(x, y, z)
+        this.mesh.position.set(x, y, z)
     }
 }
--- src/chapter04/src/systems/system_PlayerMoveOnLevel.js
+++ src/chapter04/src/systems/system_PlayerMoveOnLevel.js
@@ -2,6 +2,7 @@ import { helper_CollisionsItems_v02 } from '../../../_CORE/helpers/helper_Collis
 import * as THREE from 'three'
 
 
+
 export class system_PlayerMoveOnLevel {
     constructor (root) {
         const { 
@@ -11,25 +12,13 @@ export class system_PlayerMoveOnLevel {
             level,
         } = root
 
-        const {
-            speed,
-            speedDown,
-            speedRot,
-        } = CONSTANTS.playerConfig
-
-        const OFFSET_FROM_PLANES = 5
 
 
         /** set items to collisions */
-        const collisionsFloor = new helper_CollisionsItems_v02()
-        for (let key in level.allMeshes) {
-            collisionsFloor.setItemToCollision(level.allMeshes[key])
-        }
-
         const collisionsWalls = new helper_CollisionsItems_v02()
         for (let key in level.allMeshes) {
             if (key === 'road_wall') {
-                level.allMeshes[key].userData['isCanWalk'] = true
+                level.allMeshes[key].userData['isWallWalking'] = true
             }
 
             collisionsWalls.setItemToCollision(level.allMeshes[key])
@@ -37,134 +26,139 @@ export class system_PlayerMoveOnLevel {
 
 
 
-        let keys = {}
-        let isButtonsDisabled = false
+        const {
+            speed,
+            speedDown,
+            speedRot,
+        } = CONSTANTS.playerConfig
+
         let isBlocked = false
-        const isCanMove = {
-            'up': true,
-            'down': true,
-        }
+        player.toggleBlocked = val => isBlocked = val
+
+        let isButtonsDisabled = false
+        let keys = {}
+    
         const quaternionOld = new THREE.Quaternion()
         const quaternionNew = new THREE.Quaternion()
-        const upDefaultVector = new THREE.Vector3(0, 1, 0)
+
+        const UP_VECTOR = new THREE.Vector3(0, 1, 0)
+        const OFFSET_FROM_PLANES = 17
+        const OFFSET_FROM_PLANES_TO_DROP = 17.2
+        const OFFSET_FROM_PLANES_TO_STAIR = 16.5
+
+
+
 
 
 
         const rotatePlayerToTop = () => {
-            isCanMove['up'] = false
-            quaternionOld.copy(player._mainObj.quaternion)
-            quaternionNew.setFromAxisAngle(upDefaultVector, Math.random() * Math.PI * 2 )
-            helper_rotate(player._mainObj, quaternionOld, quaternionNew)
+            quaternionOld.copy(player.mesh.quaternion)
+            quaternionNew.setFromAxisAngle(UP_VECTOR, Math.random() * Math.PI * 2)
+
+            isBlocked = true
+            helper_rotate(player.mesh, quaternionOld, quaternionNew)
                 .then(() => {
-                    player._mainObj.up.copy(upDefaultVector)
-                    isCanMove['up'] = true
+                    player.mesh.up.copy(UP_VECTOR)
+                    isBlocked = false
                 })
         }
+
+
+
+
+        const rotatePlayerToCollisionTarget = collision => {
+            quaternionOld.copy(player.mesh.quaternion)
+            const la = new THREE.Vector3().addVectors(player.mesh.position, collision.face.normal)
+            player.mesh.lookAt(la)
+            player.mesh.up.copy(collision.face.normal)
+            player.mesh.rotateX(Math.PI / 2)
+            quaternionNew.copy(player.mesh.quaternion)
+
+            player.mesh.setRotationFromQuaternion(quaternionOld)
+
+
+            isBlocked = true
+            helper_rotate(player.mesh, quaternionOld, quaternionNew)
+                .then(() => {
+                    isBlocked = false
+                })
+        } 
+
+
+
+
+        const checkBottomAndDropDownPlayer = data => {
+            const [isCollision, collision] = collisionsWalls.checkCollisions(player.mesh, player.bottomObj, OFFSET_FROM_PLANES_TO_DROP)
+
+            /** free down without intercepts */
+            if (!isCollision) {
+                /** if player not up - rotated to up */
+                !player.mesh.up.equals(UP_VECTOR) && rotatePlayerToTop()
+                player.mesh.position.y += (speedDown * data.count)
+
+                return;
+            }
+
+            /** check is player in wall and must down - rotate to top */
+            if (
+                !collision.object.userData['isWallWalking'] &&
+                !player.mesh.up.equals(UP_VECTOR)
+            ) {
+                rotatePlayerToTop()
+                return;
+            }
+
+            /** move player to top if on stairs */
+            if (collision.distance < OFFSET_FROM_PLANES_TO_STAIR) {
+                player.mesh.translateY(OFFSET_FROM_PLANES - collision.distance)
+            }
+        }
+
+
+
+
+        const checkAndMoveFront = data => {
+            const [isCollision, collision] = collisionsWalls.checkCollisions(player.mesh, player.frontObj, OFFSET_FROM_PLANES)
+
+            if (!isCollision) {
+                player.mesh.translateZ(-speed * data.count)
+            } else if (collision.object.userData['isWallWalking']) {
+                rotatePlayerToCollisionTarget(collision)
+            }
+        }
+        
+        
+
+
+        const checkAndMoveBack = data => {
+            const [isCollision] = collisionsWalls.checkCollisions(player.mesh, player.backObj, OFFSET_FROM_PLANES)
+            if (!isCollision) return;
+                
+            player.mesh.translateZ(speed * data.count)
+        }
+
 
     
     
         const update = data => {
             if (isButtonsDisabled) return;
 
-            keys['left'] && (player._mainObj.rotateY(speedRot * data.count))
-            keys['right'] && (player._mainObj.rotateY(-speedRot * data.count))
-
+            keys['left'] && player.mesh.rotateY(speedRot * data.count)
+            keys['right'] && player.mesh.rotateY(-speedRot * data.count)
 
             if (isBlocked) return;
 
-            /** check bottom floors */
-
-            if (isCanMove['up']) {
-                const [isCollision, collision] = collisionsFloor.checkCollisions(player._mainObj, player.bottomObj, OFFSET_FROM_PLANES + 0.2)
-
-
-                /** free down without intercepts */
-                if (!isCollision) {
-
-                    /** if player not up - rotated to up */
-                    !player._mainObj.up.equals(upDefaultVector) && rotatePlayerToTop()
-
-                    player._mainObj.position.y += speedDown
-
-                /** check collisionFloor */
-                } else {
-
-                    /** check is player in wall and must down - rotate to top */
-                    if (
-                        !collision.object.userData.isCanWalk &&
-                        !player._mainObj.up.equals(upDefaultVector)
-                    ) {
-
-                        rotatePlayerToTop()
-
-                    } else {
-
-                        if (collision.distance < 4.5) {
-                            player._mainObj.translateY(OFFSET_FROM_PLANES - collision.distance)
-                        }
-                    }
-                }
-            }
-
-
-
-            /** check walls */
-            {
-                if (keys['up'] && isCanMove['up']) {
-
-
-                    const [isCollision, collision] = collisionsWalls.checkCollisions(player._mainObj, player.frontObj, OFFSET_FROM_PLANES)
-
-                    /** move player to front */
-                    if (!isCollision) {
-
-
-                        player._mainObj.translateZ(-speed * data.count)
-
-
-                    /** rotate player to wall */
-                    } else {
-
-
-                        if (collision.object.userData.isCanWalk) {
-                            if (isCanMove['up']) {
-
-                                quaternionOld.copy(player._mainObj.quaternion)
-                                const la = new THREE.Vector3().addVectors(player._mainObj.position, collision.face.normal)
-                                player._mainObj.lookAt(la)
-                                player._mainObj.up.copy(collision.face.normal)
-                                player._mainObj.rotateX(Math.PI / 2)
-                                quaternionNew.copy(player._mainObj.quaternion)
-
-                                player._mainObj.setRotationFromQuaternion(quaternionOld)
-
-
-                                isCanMove['up'] = false
-                                helper_rotate(player._mainObj, quaternionOld, quaternionNew)
-                                    .then(() => {
-                                        isCanMove['up'] = true
-                                    })
-                            }
-                        }
-                    }
-                }
-
-
-                if (keys['down']) {
-                    const [isCollision, collision] = collisionsWalls.checkCollisions(player._mainObj, player.backObj, OFFSET_FROM_PLANES)
-                    if (!isCollision) {
-                        player._mainObj.translateZ(speed * data.count)
-                    }
-                }
-
-            }
+            checkBottomAndDropDownPlayer(data)
+            keys['up'] && checkAndMoveFront(data)
+            keys['down'] && checkAndMoveBack(data)
         }
     
     
+
+
         emitter.subscribe('keyEvent')(data => keys = data)
         emitter.subscribe('frameUpdate')(update)
-        //emitter.subscribe('toggleDialog')(val => isButtonsDisabled = val.isOpen)
-
+        emitter.subscribe('toggleDialog')(val => isButtonsDisabled = val.isOpen)
     }
 } 
 
@@ -172,17 +166,18 @@ export class system_PlayerMoveOnLevel {
 
 
 
-const helper_rotate = (mesh, q1, q2) => {
-    console.log('startRotate')
-    return new Promise(res => {
+const helper_rotate = (mesh, quat1, quat2) => {
+    return new Promise(resolve => {
         let n = 0
+
         const update = () => {
             n += 0.03
             n > 1 && (n = 1)
-            mesh.quaternion.slerpQuaternions(q1, q2, n)
+
+            mesh.quaternion.slerpQuaternions(quat1, quat2, n)
 
             if (n === 1) {
-                return res()
+                return resolve()
             }
 
             setTimeout(update, 15)
@@ -191,3 +186,4 @@ const helper_rotate = (mesh, q1, q2) => {
         update()
     })
 }
+
