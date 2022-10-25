c15a72f9243ccf781d2fc260c2128b0490224885
vasilii <porokh.vs@gmail.com>
2022-10-26 00:07:13 +0300

    process

--- src/chapter05/src/constants/const_dialogs.js
+++ src/chapter05/src/constants/const_dialogs.js
@@ -4,11 +4,11 @@ const location01 = {
     phrases: [
         {
             q: 'What\'s happening?', // Что происходит?
-            a: "I am generating the Great Recursion. I need a component. Deliver it to me.", // Я генерю Великую Рекурсию. Мне нужна компонента. Доставь мне ее.
+            a: "I'm generating the Great Recursion. I need a component. Deliver it to me.", // Я генерю Великую Рекурсию. Мне нужна компонента. Доставь мне ее.
             event: 'nextReply',
         }, {
-            q: 'Good. Where can I find the component?', // Хорошо. Где мне найти компоненту?
-            a: 'I updated the sign in your car, follow it.', // Я обновил указатель в твоей машине, следуй ему.
+            q: 'Alright. Where can I find this component?', // Хорошо. Где мне найти компоненту?
+            a: 'I updated the location mark in your car, follow it.', // Я обновил указатель в твоей машине, следуй ему.
             event: 'nextReply',
         }, {
             q: 'I\'ll be back.', // Я вернусь.
@@ -27,8 +27,8 @@ const location02 = {
             a: 'I will generate you a Great Component for the Great Recursion. I need a Component.', // Я сгенерю тебе Великую Компоненту для Великой Рекурсии. Мне нужна Компонента.
             event: 'nextReply',
         }, {
-            q: 'Where can I find her?', // Где мне ее найти?
-            a: 'I updated the pointer in the car.', // Я обновил указатель в машине.
+            q: 'Where can I find it?', // Где мне ее найти?
+            a: 'I updated the location mark in the car.', // Я обновил указатель в машине.
             event: 'close',
         },
     ],
@@ -39,18 +39,18 @@ const location03 = {
     phraseIndex: 0,
     phrases: [
         {
-            q: 'Do you have a Component for the Great Component of the Great Recursion?', // У тебя есть Компонента для Великой Копоненты Великой Рекурсии?
-            a: 'Yes. I have finished generating the Component.', // Да. Я закончил генерить Компоненту.
+            q: '..Component.. ?', // У тебя есть Компонента для Великой Копоненты Великой Рекурсии?
+            a: 'Yes. I have finished generating the Component for the Great Component of the Great Recursion.', // Да. Я закончил генерить Компоненту.
             event: 'nextReply',
         },
         {
             q: "I need to deliver it to generate the Great Component.", // Мне нужно доставить ее для генерации Великой Компоненты.
-            a: 'The pointer in your car has been updated. He will point to the Component of the Great Component.', // Указатель в твоей машине обновлен. Он укажет на Компоненту Великой Компоненты.
+            a: 'The location mark in your car has been updated. It will point to the Component of the Great Component.', // Указатель в твоей машине обновлен. Он укажет на Компоненту Великой Компоненты.
             event: 'nextReply',
         },
         {
             q: "Is my car the key to Components?", // Моя машина это ключ к Компонентам?
-            a: 'Your Machine is the main Generator. Follow him.', // Твоя Машина это главный Генератор. Следуй ему.
+            a: 'Your Car is the main Iterator. Follow it.', // Твоя Машина это главный Генератор. Следуй ему.
             event: 'close',
         },
     ],
--- src/chapter05/src/constants/constants_replicies.js
+++ src/chapter05/src/constants/constants_replicies.js
@@ -1,31 +1,12 @@
 export const TRANSLATE_WORLDS = {
     'ru': {
-        // 'What is this place?': "Что это за место?",
-        // "It's The Great Way to the surface.": "Это великий путь наверх.",
-        // 'Who are you?': "Кто ты?",
-        // 'I help The Creator.': "Я помогаю создателю.",
-        // 'What are you doing?': "Что ты делаешь?",
-        // "Don't distract me, I have to dig the tunnel.": "Не отвлекай, мне надо копать туннель.",
-        // "Such enormous dungeons...": "Такие большие подземелья...",
-        // "The Creator gave the order to dig.": "Создатель приказал копать. ",
-        // "How long have you been digging?": "Как долго вы копали? ",
-        // "Time does not matter, the goal is what's important.": "Время не имеет значения, важна цель. ",
-        // "What do you do here?": "Что ты делаешь?",
-        // "Long ago, the Creator fell under the ground. He created us and gave us an assignment to dig.":"Давным-давно Творец провалился под землю. Он создал нас и дал задание копать. ",
-        // "But you already dug the way out...": "Но вы уже прорыли выход ...",
-        // "When we dug a tunnel, The Creator went through it.": "Когда мы прорыли путь, творец прошел по нему.",
-        // "And...": "И...",
-        // 'We are made to dig. And we keep on doing it. We believe that he will return to us.': "Мы созданы для того, чтобы копать. И мы продолжаем это делать. Мы верим, что он вернется к нам. ",
-
-
-
         'What\'s happening?': 
             'Что происходит?',
-        "I am generating the Great Recursion. I need a component. Deliver it to me.":
+        "I'm generating the Great Recursion. I need a component. Deliver it to me.":
              'Я генерю Великую Рекурсию. Мне нужна компонента. Доставь мне ее.',
-        'Good. Where can I find the component?': 
+        'Alright. Where can I find this component?': 
             'Хорошо. Где мне найти компоненту?',
-        'I updated the sign in your car, follow it.': 
+        'I updated the location mark in your car, follow it.': 
             'Я обновил указатель в твоей машине, следуй ему.',
         'I\'ll be back.': 
             'Я вернусь.',
@@ -36,23 +17,23 @@ export const TRANSLATE_WORLDS = {
             'У тебя есть Компонента для Великой Рекурсии?',
         'I will generate you a Great Component for the Great Recursion. I need a Component.': 
             'Я сгенерю тебе Великую Компоненту для Великой Рекурсии. Мне нужна Компонента.',
-        'Where can I find her?': 
+        'Where can I find it?': 
             'Где мне ее найти?',
-        'I updated the pointer in the car.': 
+        'I updated the location mark in the car.': 
             'Я обновил указатель в машине.',
     
-        'Do you have a Component for the Great Component of the Great Recursion?': 
-            'У тебя есть Компонента для Великой Копоненты Великой Рекурсии?',
-        'Yes. I have finished generating the Component.': 
-            'Да. Я закончил генерить Компоненту.',
+        '..Component.. ?': 
+            '..Компонента..?',
+        'Yes. I have finished generating the Component for the Great Component of the Great Recursion.': 
+            'Да. Я закончил генерить Компоненту для Великой Компоненты для Великой Рекурсии.',
         "I need to deliver it to generate the Great Component.": 
             'Мне нужно доставить ее для генерации Великой Компоненты.',
-        'The pointer in your car has been updated. He will point to the Component of the Great Component.': 
+        'The location mark in your car has been updated. It will point to the Component of the Great Component.': 
             'Указатель в твоей машине обновлен. Он укажет на Компоненту Великой Компоненты.',
         "Is my car the key to Components?": 
             'Моя машина это ключ к Компонентам?',
-        'Your Machine is the main Generator. Follow him.': 
-            'Твоя Машина это главный Генератор. Следуй ему.',
+        'Your Car is the main Iterator. Follow it.': 
+            'Твоя Машина это главный Итератор. Следуй ему.',
 
 
         'open dialog': 'начать диалог',
@@ -66,4 +47,3 @@ export const TRANSLATE_WORLDS = {
         'link': 'ссылка',
     }
 }
-
